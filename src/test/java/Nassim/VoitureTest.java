b479b4ffe06eb1f093c530d04e50823653cc3a0d
Manseur <nassim.manseur@edu.univ-paris13.fr>
2024-06-17 16:21:25 +0200

    Ajout de test pour un coverage complet TP2

--- src/test/java/Nassim/VoitureTest.java
+++ src/test/java/Nassim/VoitureTest.java
@@ -3,6 +3,7 @@ package Nassim;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class VoitureTest {
     @Test
     public void testVoiture() {
